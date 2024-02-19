5a198a52892a260bb0af06fdf8e5e85d8618affe
mahesboopathi <mahesboopathi25@gmail.com>
2024-02-19 20:31:21 +0530

    first commit

---
+++ src/test/java/com/orangeHRM/Pages/LoginPage.java
@@ -0,0 +1,46 @@
+package com.orangeHRM.Pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+
+public class LoginPage {
+
+	WebDriver driver;
+
+	public LoginPage(WebDriver driver) {
+		// TODO Auto-generated constructor stub
+
+		this.driver = driver;
+	}
+
+	// Locators..
+	By usename = By.name("username");
+	By passwor = By.name("password");
+	By login = By.xpath("//button[@type='submit']");
+	// By profile = By.xpath("//div[@class='oxd-brand-banner']");
+
+	By profile = By.xpath("//div[@class='oxd-sidepanel-body']//ul//li[1]//span");
+
+	public void userName(String uid) {
+
+		driver.findElement(usename).sendKeys(uid);
+	}
+
+	public void passWord(String pass) {
+		driver.findElement(passwor).sendKeys(pass);
+	}
+
+	public void submit() {
+		driver.findElement(login).click();
+	}
+
+	/*
+	 * public boolean homepage() { return driver.findElement(profile).isDisplayed();
+	 * }
+	 */
+
+	public String profilePage() {
+
+		return driver.findElement(profile).getText();
+	}
+}
---
+++ src/test/java/com/orangeHRM/testcases/LoginTest.java
@@ -0,0 +1,63 @@
+package com.orangeHRM.testcases;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.orangeHRM.Pages.LoginPage;
+
+public class LoginTest {
+
+	WebDriver dr;
+	LoginPage lp;
+
+	@BeforeMethod
+	public void browserSetUp() {
+
+		dr = new ChromeDriver();
+		dr.manage().window().maximize();
+
+		dr.get("https://opensource-demo.orangehrmlive.com/");
+//dr.manage().timeouts().imn
+	}
+
+	@Test
+	public void validUserPass() {
+		lp = new LoginPage(dr);
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		lp.userName("Admin");
+		try {
+			Thread.sleep(500);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		lp.passWord("admin123");
+		lp.submit();
+		try {
+			Thread.sleep(1000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		Assert.assertEquals(lp.profilePage(), "Admin", "Title not matched");
+	}
+
+	@AfterMethod
+	public void browserClose() {
+
+		dr.quit();
+	}
+
+}
